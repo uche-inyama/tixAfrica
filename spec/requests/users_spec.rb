ce134aed18c11c185ac7daf31db626b78fcf9faa
uche-inyama <depoulet@yahoo.com>
2022-01-06 08:30:16 +0100

    write request test for registering users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,1 +1,16 @@
+require 'rails_helper'
 
+RSpec.describe "Users", type: :request do
+  describe "POST /users, Registers a user" do
+    let(:params) do 
+      { 
+        user: { email: 'xyz@gmail.com', password: 'xxxxxxx' }
+      }
+    end
+    before { post "/users", params: params }
+
+    it 'returns status code 302' do
+      expect(response).to have_http_status(302)
+    end
+  end
+end
